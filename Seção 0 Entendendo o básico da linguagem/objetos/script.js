4b3072de93b1e7549067e58e446bdfc2dad8f0d9
Michelangelo Giuseppe Pezzini <lds.pezzini@gmail.com>
2022-07-11 22:05:21 -0300

    0

---
+++ Seção 0 Entendendo o básico da linguagem/objetos/script.js
@@ -0,0 +1,9 @@
+const notebook = {
+  marca: "acer",
+  modelo: "Nitro 5",
+  preço: 5000,
+  novidades: function () {
+    document.getElementById("texto").innerHTML =
+      "Este é o melhor notebook gamer";
+  },
+};
